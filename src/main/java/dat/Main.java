3111068f370d4f7499ad9aacb0d5f3089ef028b9
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-10-07 10:23:44 +0200

    Update to fix workflow actions

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-
+    // Update for fixing workflow
     public static void main(String[] args) {
         ApplicationConfig.startServer(7070);
     }
